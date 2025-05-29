de8809a951d23a491d6d77ceaaaf5672a1256989
Otengtech <otengebenezer326@gmail.com>
2025-05-29 01:23:48 -0700

    commit

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sneekers/',
+  base: '/',
   server: {
     historyApiFallback: true,  // Fixes refresh issue
   }
