b95f55648298e14d6621126e140603f06fdcd701
Otengtech <otengebenezer326@gmail.com>
2025-07-23 13:53:04 -0700

    commit

--- Backend/email.js
+++ Backend/email.js
@@ -41,7 +41,7 @@ app.use(cors({
   credentials: true,
 }));
 
-app.options("*", cors());
+// app.options("*", cors());
 // ---------- Connect DB ----------
 connectDB();
 
--- Backend/routes/paymentRoutes.js
+++ Backend/routes/paymentRoutes.js
@@ -2,6 +2,9 @@ import express from 'express';
 import { initializePayment, handleWebhook } from '../controllers/paymentController.js';
 
 const router = express.Router();
-router.post('/pay', initializePayment);
+
+// ✅ Remove "/pay" — it's already in app.use('/api/pay')
+router.post('/', initializePayment);
 router.post('/webhook', handleWebhook);
+
 export default router;
