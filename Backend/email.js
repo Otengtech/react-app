d3a6cc6242fe65505e8af439850e1f7f02ff2628
Otengtech <otengebenezer326@gmail.com>
2025-07-22 19:09:11 -0700

    another commit

--- Backend/email.js
+++ Backend/email.js
@@ -18,6 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // ---------- Middleware ----------
+app.use(express.json());
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
@@ -25,9 +26,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // CORS
+const allowedOrigins = ['https://react-app-7wev.onrender.com']; 
+
 app.use(cors({
-  origin: 'https://react-app-7wev.onrender.com',
-  methods: ['GET', 'POST'],
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
 }));
 
